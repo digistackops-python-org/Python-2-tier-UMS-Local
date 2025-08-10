5ea5cefac5161236c78793de75fbd803c3c17121
venkatesh peta <85110131+techizone@users.noreply.github.com>
2025-08-10 22:04:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 
 # Read DB credentials directly from environment variables
 db = mysql.connector.connect(
-    host=os.getenv("DB_HOST", "localhost"),
-    user=os.getenv("DB_USER", "root"),
-    password=os.getenv("DB_PASS", ""),
-    database=os.getenv("DB_NAME", "test")
+    host=os.getenv("MYSQL_HOST", "localhost"),
+    user=os.getenv("MYSQL_USER", "root"),
+    password=os.getenv("MYSQL_PASSWORD", ""),
+    database=os.getenv("MYSQL_DATABASE", "test")
 )
 cursor = db.cursor()
 
