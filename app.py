6208a8825bec9bb730f2c6969cf0196531e66085
venkatesh peta <85110131+techizone@users.noreply.github.com>
2025-08-10 22:12:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ db = mysql.connector.connect(
     host=os.getenv("MYSQL_HOST", "localhost"),
     user=os.getenv("MYSQL_USER", "root"),
     password=os.getenv("MYSQL_PASSWORD", ""),
-    database=os.getenv("MYSQL_DATABASE", "test")
+    database=os.getenv("MYSQL_DATABASE", "user")
 )
 cursor = db.cursor()
 
