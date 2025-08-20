8bdb83acdf02ec3d7fe166cb314e1b2855c70aa0
venkatesh peta <85110131+DigiStackOps@users.noreply.github.com>
2025-08-20 19:47:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 DATABASE_CONFIG = {
-    'host': os.getenv('DB_HOST', '127.0.0.1'), 
-    'user': os.getenv('DB_USER', 'appuser'),
-    'password': os.getenv('DB_PASS', ''),
-    'database': os.getenv('DB_NAME', 'user')
+    'host': os.getenv('MYSQL_HOST', '127.0.0.1'), 
+    'user': os.getenv('MYSQL_USER', 'appuser'),
+    'password': os.getenv('MYSQL_PASSWORD', ''),
+    'database': os.getenv('MYSQL_DATABASE', 'user')
 }
